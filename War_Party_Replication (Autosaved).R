7ecd50f6fa95ba929c4245aa22c6d3b7f640d8b7
Andrew Bertoli <abertoli@berkeley.edu>
2016-08-13 13:57:48 -0700

    Updated replication file

--- War_Party_Replication (Autosaved).R
+++ War_Party_Replication (Autosaved).R
@@ -1,3 +1,4 @@
+library(reshape)
 library(rdrobust)
 library(cowplot)
 require(ggplot2)
@@ -10,7 +11,18 @@ setwd("/Users/andrewbertoli/Dropbox/Electoral-RDs/2Empirics/Data")
 
 data=read.csv("Election_Data_Updated.csv")
 
-data$Z=(data$PresVotes - data$PresSecondVotes)/(data$PresVotes + data$PresSecondVotes)
+#data[data$Country=="Cyprus"&data$Year==1993,]$HighDisputesInitiated=0.40
+#data[data$Country=="Cyprus"&data$Year==1993,]$DisputesInitiated=0.40
+#data[data$Country=="Cyprus"&data$Year==1998,]$PreviousHighDisputesInitiated=0.40
+#data[data$Country=="Cyprus"&data$Year==1998,]$PreviousTermHighDisputesInitiated=0.40
+#data[data$Country=="Cyprus"&data$Year==1998,]$PreviousDisputesInitiated=0.40
+#data[data$Country=="Cyprus"&data$Year==1998,]$PreviousTermDisputesInitiated=0.40
+
+#data[data$Country=="Taiwan"&data$Year==2004,]$DisputesInitiated=0.25
+#data[data$Country=="Taiwan"&data$Year==2004,]$PreviousHighDisputesInitiated=0.25
+#data[data$Country=="Taiwan"&data$Year==2004,]$PreviousTermHighDisputesInitiated=0.25
+
+#data$Z=(data$PresVotes - data$PresSecondVotes)/(data$PresVotes + data$PresSecondVotes)
 
 setwd("/Users/andrewbertoli/Dropbox/Electoral-RDs/1Drafts/WarParty")
 
@@ -45,6 +57,98 @@ dev.off()
 
 
 close=dems[abs(dems$Z)<=0.04,]
+
+
+# BalancePlot
+
+covs=c("PreviousDisputesInitiated","PreviousHighDisputesInitiated","AllPreviousDisputes","AllPreviousHighDisputes","PreviousRevisionistDisputes","PreviousHighRevisionistDisputes", "irst", "milex", "milper", "pec", "tpop", "upop")
+
+est=matrix(NA,ncol=3,nrow=length(covs))
+
+for(i in 1:length(covs)){
+output=t.test(dems[abs(dems$Z)<=0.04,covs[i]]~as.numeric(dems[abs(dems$Z)<=0.04,]$Z>0))
+est[i,]=c(output$estimate[2]-output$estimate[1],-output$conf.int[1],-output$conf.int[2])/sd(dems[,covs[i]],na.rm=TRUE)}
+
+
+theme_nolegend <- function (base_size = 9, base_family = "", height, width) 
+{
+  theme_grey(base_size = base_size, base_family = base_family) %+replace% 
+    theme(axis.text = element_text(size = rel(0.8)), 
+          legend.position="none", 
+          axis.ticks = element_line(colour = "black"), 
+          legend.key = element_rect(colour = "grey80"), 
+          panel.background = element_rect(fill = "white", colour = NA), 
+          panel.border = element_rect(fill = NA,colour = "grey50"), 
+          panel.grid.major = element_line(colour = "grey90", size = 0.2), 
+          panel.grid.minor = element_line(colour = "grey98", size = 0.5), 
+          strip.background = element_rect(fill = "grey80",  colour = "grey50"), 
+          strip.background = element_rect(fill = "grey80", colour = "grey50"))
+}
+
+cd <- as.data.frame(matrix(NA,length(covs),5))
+conditions <- c("Previous Disputes Initiated","Previous High-Level Disputes Initiated","All Previous Disputes","All Previous High-Level Disputes","Previous Revisionist Disputes", "Previous High-Level Revisionist Disputes", "Iron and Steel Production",  "Military Expenditures", "Military Personel",     "Energy Consumption",  "Total Population", "Urban Population")
+names(cd) <- c("mean","upper","lower","ord","measure")
+cd$mean <- as.numeric(est[,1])
+cd$lower <- as.numeric(est[,3])
+cd$upper <- as.numeric(est[,2])
+cd$ord <- c(length(covs):1)
+cd$measure <- factor(conditions, levels=conditions[order(cd$ord)])
+# make the graph
+library(ggplot2)
+
+f <- ggplot(cd, 
+            aes(x=mean,y=measure,color=measure))
+balanceplot1 <- f+geom_vline(xintercept=0, linetype="longdash")+
+
+  geom_errorbarh(aes(xmax =  upper, 
+                     xmin = lower),
+                 size=1.5, height=0)+
+  geom_point(stat="identity",size=4,fill="white")+
+  xlab("Difference (Standardized)")+ylab("")+ labs(title="Ideology") +  theme_nolegend()+theme(axis.text=element_text(size=10),axis.title=element_text(size=12),axis.title.x = element_text(hjust=1),plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))+xlim(-2,2)
+
+balanceplot1
+
+
+
+
+# External Validity
+
+colnames(nmc2)=c("Urban Population", "Total Population", "Energy Consumption", "Military Personel",  "Military Expenditures","Iron and Steel Production")
+
+# for(i in 1:ncol(nmc2)){nmc2[,i]=nmc2[,i]/mean(nmc2[,i],na.rm=TRUE)}
+
+for(i in 1:ncol(nmc2)){nmc2[,i]=log(nmc2[,i]+1)}
+
+sample2=close[,c("upop", "tpop", "pec", "milper",  "milex","irst")]
+
+for(i in 1:ncol(sample2)){sample2[,i]=log(sample2[,i]+1)}
+
+
+fill=matrix(NA,nrow=nrow(nmc2)-nrow(sample2),ncol=ncol(sample2))
+
+colnames(fill)=c("Urban Population", "Total Population", "Energy Consumption", "Military Personel",  "Military Expenditures","Iron and Steel Production")
+
+sample2=rbind(sample2,fill)
+
+caps=cbind(sample2[,1],nmc2[,1],sample2[,2],nmc2[,2],sample2[,3],nmc2[,3],sample2[,4],nmc2[,4],sample2[,5],nmc2[,5],sample2[,6],nmc2[,6])
+
+caps=cbind(sample2[,1],nmc2[,1],sample2[,2],nmc2[,2],sample2[,3],nmc2[,3],sample2[,4],nmc2[,4],sample2[,5],nmc2[,5],sample2[,6],nmc2[,6])
+
+colnames(caps)=c("Sample Urban Population","Population Urban Population", "Sample Total Population", "Population Total Population","Sample Energy Consumption", "Population Energy Consumption","Sample Military Personel","Population Military Personel","Sample Military Expenditures",  "Population Military Expenditures","Sample Iron and Steel Production","Population Iron and Steel Production")
+
+caps=melt(caps)
+
+colnames(caps)[2:3]=c("Variable","Value")
+
+caps$Variable=factor(caps$Variable,levels=c("Sample Urban Population","Population Urban Population", "Sample Total Population", "Population Total Population","Sample Energy Consumption", "Population Energy Consumption","Sample Military Personel","Population Military Personel","Sample Military Expenditures",  "Population Military Expenditures","Sample Iron and Steel Production","Population Iron and Steel Production"),ordered=TRUE)
+
+ExternalValidity1 = ggplot(caps, aes(Variable,Value)) + geom_boxplot(fill=rep(c("cornflowerblue","lightgrey"),6)) + coord_flip() + ylab("ln(value)") + xlab("") + theme_bw() +theme(axis.title=element_text(size=16)) + ggtitle("Ideology") +theme(plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))
+
+ExternalValidity1
+
+
+
+
 
 close[close$Z>0,c("Country","Year","DisputesInitiated","HighDisputesInitiated")][order(close[close$Z>0,]$Year),]
 
@@ -82,21 +186,6 @@ rownames(standardized_results)=outcomes
 
 t_test_results=standardized_results
 
-theme_nolegend <- function (base_size = 9, base_family = "", height, width) 
-{
-  theme_grey(base_size = base_size, base_family = base_family) %+replace% 
-    theme(axis.text = element_text(size = rel(0.8)), 
-          legend.position="none", 
-          axis.ticks = element_line(colour = "black"), 
-          legend.key = element_rect(colour = "grey80"), 
-          panel.background = element_rect(fill = "white", colour = NA), 
-          panel.border = element_rect(fill = NA,colour = "grey50"), 
-          panel.grid.major = element_line(colour = "grey90", size = 0.2), 
-          panel.grid.minor = element_line(colour = "grey98", size = 0.5), 
-          strip.background = element_rect(fill = "grey80",  colour = "grey50"), 
-          strip.background = element_rect(fill = "grey80", colour = "grey50"))
-}
-
 cd <- as.data.frame(matrix(NA,length(outcomes),6))
 conditions <- c("Disputes Initiated","High-Level Disputes Initiated","All Disputes","All High-Level Disputes","Revisionist Disputes","High-Level Revisionist Disputes")
 names(cd) <- c("mean","upper","lower","ord","measure")
@@ -116,7 +205,7 @@ plot3 <- f+geom_vline(xintercept=0, linetype="longdash")+
                      xmin = lower),
                  size=1.5, height=0)+
   geom_point(stat="identity",size=4,fill="white")+
-  xlab("Estimated Treatment Effect (Standardized)")+ylab("")+ labs(title="") +  theme_nolegend() +theme(axis.text=element_text(size=10),axis.title=element_text(size=12.5),plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))+scale_x_continuous(breaks=c(-0.5,0,0.2,0.5,0.8,1),labels=c("-0.5","0","small","medium","large","1")) # Aggression During Term
+  xlab("Estimated Treatment Effect (Standardized)")+ylab("")+ labs(title="") +  theme_nolegend() +theme(axis.text=element_text(size=8.2),axis.title=element_text(size=12.5),plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))+scale_x_continuous(breaks=c(-0.5,0,0.2,0.5,0.8,1),labels=c("-0.5","0","small (0.2)","medium (0.5)","large (0.8)","1")) # Aggression During Term
 
 ggsave("AggressionPlotIdeo.pdf",width=4,height=2,scale = 1.6)
 
@@ -174,6 +263,51 @@ dems$Z=-dems$Z*(2*as.numeric(dems$WinnerPartyInc-dems$RunnerUpPartyInc>0)-1)
 
 dems$T=as.numeric(dems$Z>0)
 
+# Balance Plot
+
+covs=c("PreviousDisputesInitiated","PreviousHighDisputesInitiated","AllPreviousDisputes","AllPreviousHighDisputes","PreviousRevisionistDisputes","PreviousHighRevisionistDisputes", "irst", "milex", "milper", "pec", "tpop", "upop")
+
+est=matrix(NA,ncol=3,nrow=length(covs))
+
+for(i in 1:length(covs)){
+output=t.test(dems[abs(dems$Z)<=0.04,covs[i]]~as.numeric(dems[abs(dems$Z)<=0.04,]$Z>0))
+est[i,]=c(output$estimate[2]-output$estimate[1],-output$conf.int[1],-output$conf.int[2])/sd(dems[,covs[i]],na.rm=TRUE)}
+
+cd <- as.data.frame(matrix(NA,length(covs),5))
+conditions <- c("Previous Disputes Initiated","Previous High-Level Disputes Initiated","All Previous Disputes","All Previous High-Level Disputes","Previous Revisionist Disputes", "Previous High-Level Revisionist Disputes", "Iron and Steel Production",  "Military Expenditures", "Military Personel",     "Energy Consumption",  "Total Population", "Urban Population")
+names(cd) <- c("mean","upper","lower","ord","measure")
+cd$mean <- as.numeric(est[,1])
+cd$lower <- as.numeric(est[,3])
+cd$upper <- as.numeric(est[,2])
+cd$ord <- c(length(covs):1)
+cd$measure <- factor(conditions, levels=conditions[order(cd$ord)])
+# make the graph
+library(ggplot2)
+
+f <- ggplot(cd, 
+            aes(x=mean,y=measure,color=measure))
+plot1 <- f+geom_vline(xintercept=0, linetype="longdash")+
+
+  geom_errorbarh(aes(xmax =  upper, 
+                     xmin = lower),
+                 size=1.5, height=0)+
+  geom_point(stat="identity",size=4,fill="white")+
+  xlab("Difference (Standardized)")+ylab("")+ labs(title="Incumbency") +  theme_nolegend()+theme(axis.text=element_text(size=10),axis.title=element_text(size=12),axis.title.x = element_text(hjust=1),plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))+xlim(-2,2)
+
+balanceplot2
+
+plot_grid(balanceplot1,balanceplot2,ncol=2)
+
+ggsave("PlaceboPlotsWarParty.pdf",width=3.2,height=1.2,scale = 3)
+
+
+# ExternalValidity
+
+
+
+
+
+
 dems$AbsoluteChangeDisputesInitiated=with(dems,abs(DisputesInitiated-PreviousTermDisputesInitiated))
 dems$AbsoluteChangeHighDisputesInitiated=with(dems,abs(HighDisputesInitiated-PreviousTermHighDisputesInitiated))
 dems$AbsoluteChangeAllDisputes=with(dems,abs(AllDisputes-AllPreviousTermDisputes))
@@ -197,6 +331,8 @@ dev.off()
 plot_grid(m,m2,ncol=2)
 
 ggsave("ForcingDensity.pdf",width=2.4,height=1.1,scale = 3)
+
+
 
 close=dems[abs(dems$Z)<=0.04,]
 
@@ -271,7 +407,7 @@ plot3 <- f+geom_vline(xintercept=0, linetype="longdash")+
                      xmin = lower),
                  size=1.5, height=0)+
   geom_point(stat="identity",size=4,fill="white")+
-  xlab("Estimated Treatment Effect (Standardized)")+ylab("")+ labs(title="") +  theme_nolegend() + theme(axis.text=element_text(size=10),axis.title=element_text(size=12.5),plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))+ scale_x_continuous(breaks=c(-0.5,0,0.2,0.5,0.8,1),labels=c("-0.5","0","small","medium","large","1")) # Aggression During Term
+  xlab("Estimated Treatment Effect (Standardized)")+ylab("")+ labs(title="") +  theme_nolegend() + theme(axis.text=element_text(size=8.7),axis.title=element_text(size=12.5),plot.title = element_text(lineheight=1.8,size=rel(1.5),face="bold"))+ scale_x_continuous(breaks=c(-0.5,0,0.2,0.5,0.8,1),labels=c("-0.5","0","small (0.2)","medium (0.5)","large (0.8)","1")) # Aggression During Term
 
 ggsave("AggressionPlotInc.pdf",width=4,height=2,scale = 1.6)
 
