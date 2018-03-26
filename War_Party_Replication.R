d7ec527f4a76da44f30c3d3f3b5a20e03ec2d292
Andrew Bertoli <abertoli@dartmouth.edu>
2018-03-26 18:37:20 -0400

    Changed data source to WarPartyData

--- War_Party_Replication.R
+++ War_Party_Replication.R
@@ -11,7 +11,7 @@ source_url("https://raw.githubusercontent.com/AndrewBertoli/Natural-Experiments/
 
 # Read the data from Github
 
-data=read.csv("https://raw.githubusercontent.com/AndrewBertoli/War_Party_Replication/master/Data.csv",stringsAsFactors=FALSE)
+data=read.csv("https://raw.githubusercontent.com/AndrewBertoli/War_Party_Replication/master/WarPartyData.csv",stringsAsFactors=FALSE)
 
 # Create the forcing variable (distance each case was from the cut-point)
 
